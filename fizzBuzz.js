68b71ca576a767cf54e4d1f4b41d81999530093b
Y086047 <thomas.rauhofer@generali.com>
2020-01-31 08:58:39 +0100

    neue aufgabe

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -12,9 +12,55 @@
 // dann möchte ich dass du drüber nachdenkst wie man das schöner machen kann. für jede möglichkeit ein if statement welches dann logt is ja gut aber es geht noch besser.
 // ich will dann auch noch zahlen hinzufügen nämlich vielfache von 4 geben Bumm aus und vielfache von 6 geben Hamm aus. und wieder wenn mehrere teiler treffen dann gibst mehrere worte aus
 
-for (var i = 1; i <= 100; i++) {
-  if (i % 15 == 0) console.log("FizzBuzz");
-  else if (i % 3 == 0) console.log("Fizz");
-  else if (i % 5 == 0) console.log("Buzz");
-  else console.log(i);
+/* TODO
+1) const o. var
+2) variablen namen bissl übverdenken
+3) string füllen mit ifs
+
+
+
+4) vars in arrays
+*/
+
+var u = 4; //Bumm
+var v = 6; //Hamm
+var x = 100;
+var y = 3; //Fizz
+var z = 5; //Buzz
+for (var i = 1; i <= x; i++) {
+  switch (true) {
+    case i % u == 0 && i % v == 0 && i % y == 0 && i % z == 0:
+      console.log("FizzBuzzBummHamm");
+      break;
+    case i % u == 0 && i % v == 0 && i % y == 0:
+      console.log("FizzBummHamm");
+      break;
+    case i % v == 0 && i % y == 0 && i % z == 0:
+      console.log("FizzBuzzHamm");
+      break;
+    case i % y == 0 && i % z == 0 && i % u == 0:
+      console.log("FizzBuzzBumm");
+      break;
+    case i % y == 0 && i % z == 0:
+      console.log("FizzBuzz");
+      break;
+    case i % v == 0 && i % y == 0:
+      console.log("FizzHamm");
+      break;
+    case i % y == 0:
+      console.log("Fizz");
+      break;
+    case i % z == 0:
+      console.log("Buzz");
+      break;
+    case i % u == 0:
+      console.log("Bumm");
+      break;
+    case i % v == 0:
+      console.log("Hamm");
+      break;
+    default:
+      console.log(i);
+      break;
+  }
 }
