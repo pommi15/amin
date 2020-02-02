12de9f54066a5ab6e15ad452020f104d4d4fe7a4
Thomas Rauhofer <pommi15@hotmail.com>
2020-02-02 15:04:25 +0100

    Added diamond

---
+++ diamond.js
@@ -0,0 +1,25 @@
+/*
+Bau eine function der du eine ungerade zahl n übergibst und die dann einen diamanten ausgibt wobei n die zahl der zeilen ist
+
+wenn du der function 1 übergibst, kommt das raus:
+#
+
+3:
+ #
+###
+ #
+
+10:
+     #
+    ###
+   #####
+  #######
+ #########
+###########
+ #########
+  #######
+   #####
+    ###
+     #
+
+*/
