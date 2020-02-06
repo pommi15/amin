7577e27a1fef6c8421777b7e379d1af86bcdc9e5
Y086047 <thomas.rauhofer@generali.com>
2020-02-06 11:33:48 +0100

    loops

---
+++ loops.js
@@ -0,0 +1,43 @@
+/*
+1) mach einen loop der alle zahlen von eins bis 10 ausgibt, untereinander:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+2) mach einen loop der alle zahlen von 1 bis 10 ausgibt, in einer Ziele:
+12345678910
+3) mach einen loop der alle zahlen von 10 bis -10 ausgibt in einer Zeile
+109876543210-1-2-3-4-5-6-7-8-9-10
+4) mach einen loop der die fibonaccisequenz bis 100 ausgibt, untereinnander:
+0
+1
+1
+2
+3
+5
+usw.
+5) mach eine function der du einen string übergeben kannst die diesen string verkehrt ausgibt
+reverser("Hallo!") // gibt "!ollaH" aus
+6) mach einen loop der nur gerade zahlen bis 10 augibt
+7) mach eine function die eine zahl n und eine zahl k nimmt. n ist die zahl bis zu der der loop zählt und k ist die zahl durch die die geloggten zahlen teilbahr sein sollen
+teilbahr(30,3); gibt folgendes aus:
+3
+6
+9
+12
+15
+18
+21
+24
+27
+30
+8)
+
+
+*/
