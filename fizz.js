7c102fd0e79b482be56de7756b1f5fc5089d64d4
Y086047 <thomas.rauhofer@generali.com>
2020-02-06 10:32:24 +0100

    More result

--- fizz.js
+++ fizz.js
@@ -1,12 +1,12 @@
-const names = ["Fizz", "Buzz", "Bumm", "Damn"]
-const nums = [2, 3, 4, 5]
-const max = 100
-for (let i = 0; i < max; i++) {
-    let str = ""
-    for (let j = 0; j < names.length; j++) {
-        if (i % nums[j] == 0) {
-            str += names[j] + " "
-        }
+const names = ["A", "r", "m", "k", "p", "f", "i", "l", "n"];
+const nums = [2, 3, 5, 6, 7, 9, 10, 15, 20];
+const max = 100;
+for (let i = 0; i <= max; i++) {
+  let str = "";
+  for (let j = 0; j < names.length; j++) {
+    if (i % nums[j] == 0) {
+      str += names[j];
     }
-    console.log(str === "" ? i : str);
+  }
+  console.log(str === "" ? i : str);
 }
