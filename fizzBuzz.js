df1d804b7393a96ad2222cd6dad627c7e18982d6
Y086047 <thomas.rauhofer@generali.com>
2020-02-06 15:14:18 +0100

    Fizzbuzz done

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,77 +1,17 @@
-/* TODO
-1) const o. var
-2) variablen namen bissl übverdenken
-3) string füllen mit ifs
+/* Done */
+const str = ["A", "r", "m", "k", "p", "f", "i", "l", "n"];
 
-
-
-4) vars in arrays
-*/
-
-// 3 Fizz
-// 4 Bumm
-// 5 Buzz
-// 6 Hamm
-
-const str = ["Fizz", "Bumm", "Buzz", "Hamm"];
-const num = [3, 4, 5, 6,];
+const num = [2, 3, 5, 6, 7, 9, 10, 15, 20];
 const x = 100;
 let i;
 let j;
 
 for (i = 1; i <= x; i++) {
-    var m = "";
-    for (j = 0; j <= num.length; j++) {
-        if (i % num[j] == 0) {
-            m += str[j];
-        }
+  var m = "";
+  for (j = 0; j <= num.length; j++) {
+    if (i % num[j] == 0) {
+      m += str[j];
     }
-    console.log(m.length ? m : i)
+  }
+  console.log(m.length ? m : i);
 }
-
-
-
-
-
-
-
-
-
-
-// const x = 100;
-// const fizz = 3; //Fizz
-// const buzz = 5; //Buzz
-// const hamm = 6; //Hamm
-// const bumm = 4; //Bumm
-
-// for (var i = 1; i <= x; i++) {
-//     var m = "";
-//     if (i % fizz == 0) {
-//         m += "Fizz";
-
-//     } if (i % buzz == 0) {
-//         m += "Buzz";
-
-//     }
-//     if (i % bumm == 0) {
-//         m += "Bumm";
-
-//     }
-//     if (i % hamm == 0) {
-//         m += "Hamm";
-
-
-//     }
-//     if (m.length) {
-//         console.log(m.length ? m : i);
-
-//     } else {
-//         console.log(i);
-
-//     }
-
-// }
-
-
-
-
