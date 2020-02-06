f818b83bd9e9ba6e191d2a39d0f7d0e35484b8f4
Amin Mejri <ahmed-amin.mejri@generali.com>
2020-02-06 13:31:37 +0100

    fizzbuzz with arrays

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -8,43 +8,69 @@
 4) vars in arrays
 */
 
+// 3 Fizz
+// 4 Bumm
+// 5 Buzz
+// 6 Hamm
 
-
-
-
+const str = ["Fizz", "Bumm", "Buzz", "Hamm"];
+const num = [3, 4, 5, 6,];
 const x = 100;
-const fizz = 3; //Fizz
-const buzz = 5; //Buzz
-const hamm = 6; //Hamm
-const bumm = 4; //Bumm
+let i;
+let j;
 
-for (var i = 1; i <= x; i++) {
+for (i = 1; i <= x; i++) {
     var m = "";
-    if (i % fizz == 0) {
-        m += "Fizz" + "";
-
-    } if (i % buzz == 0) {
-        m += "Buzz";
-
+    for (j = 0; j <= num.length; j++) {
+        if (i % num[j] == 0) {
+            m += str[j];
+        }
     }
-    if (i % bumm == 0) {
-        m += "Bumm";
-
-    }
-    if (i % hamm == 0) {
-        m += "Hamm";
-
-
-    }
-    if (m.length) {
-        console.log(m.length ? m : i);
-
-    } else {
-        console.log(i);
-
-    }
-
+    console.log(m.length ? m : i)
 }
+
+
+
+
+
+
+
+
+
+
+// const x = 100;
+// const fizz = 3; //Fizz
+// const buzz = 5; //Buzz
+// const hamm = 6; //Hamm
+// const bumm = 4; //Bumm
+
+// for (var i = 1; i <= x; i++) {
+//     var m = "";
+//     if (i % fizz == 0) {
+//         m += "Fizz";
+
+//     } if (i % buzz == 0) {
+//         m += "Buzz";
+
+//     }
+//     if (i % bumm == 0) {
+//         m += "Bumm";
+
+//     }
+//     if (i % hamm == 0) {
+//         m += "Hamm";
+
+
+//     }
+//     if (m.length) {
+//         console.log(m.length ? m : i);
+
+//     } else {
+//         console.log(i);
+
+//     }
+
+// }
 
 
 
