e1a6acc9bf4c674d5ace40644e0008386acdcd99
Y086047 <thomas.rauhofer@generali.com>
2020-02-07 08:19:19 +0100

    Added string manipulation

--- stringManipulation.js
+++ stringManipulation.js
@@ -1,7 +1,75 @@
-/* schreib eine function die einen Namen übernimmt und dann "Hallo {name}, was läuft?" ausgibt */
+/*1) schreib eine function die einen Namen übernimmt und dann "Hallo {name}, was läuft?" ausgibt */
 
 function hallo(name) {
   // dein code hier
 }
 
-hallo("Amin"); // "sollte Hallo amin, was läuft?"" loggen
+hallo("Amin"); // sollte "Hallo amin, was läuft?"" loggen
+
+/*2)
+    mach eine function die zwei strings zusammenführt als würde man katern mischen. also immer ein zichen vom einen dann ein zeichen vom anderen.
+    das ergebnis soll retunred werden
+    compiner("test","aaaa");  würde "taeasata" zurückgeben
+
+    */
+
+function combiner(a, b) {
+  return ""; // das is nur ein platzhalter dass du ans return denkst
+}
+
+/* 3)
+mach eine function die von einem string das quadrat loggt
+das soll so aussehen:
+
+squarer("test") gibt folgendes aus:
+
+test
+test
+test
+test
+
+oder
+
+sqarer("ich") gibt folgendes aus:
+
+ich
+ich
+ich
+
+*/
+function squarer(str) {}
+/* 
+4) kopiere die squarer function nenne sie squarer1 und gib ihr noch einen parameter diagonal der ein bool ist wodurch sich der output dann so verändert:
+
+squarer("test",true) gibt folgendes aus:
+
+test
+ttes
+stte
+estt
+
+oder
+
+sqarer("ich"true) gibt folgendes aus:
+
+ich
+hic
+chi
+
+
+squarer("test",false) gibt folgendes aus:
+
+test
+test
+test
+test
+
+oder
+
+sqarer("ich",false) gibt folgendes aus:
+
+ich
+ich
+ich
+ */
+function squarer1(str, diagonal) {}
