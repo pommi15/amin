8ef05e7eb63f82f642144a2c1384a1557749ef03
amin900 <a.mejri@gmx.at>
2024-11-14 21:32:15 +0100

    Update numberSystems.js

--- numberSystems.js
+++ numberSystems.js
@@ -3,3 +3,8 @@ bau eine function die eine zahl in dezimal übernimmt und dann als binär ausgib
 also bei 10:
 1010
 */
+function decimalToBinary(decimal: number): string {
+  return decimal.toString(2);
+}
+
+console.log(decimalToBinary(10));
